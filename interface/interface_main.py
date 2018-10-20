42b05835e515075f7178ddbe30cd22aae9739fac
samb <sborges001@gmail.com>
2018-10-20 15:20:34 -0400

    added em models. added while loop until user enters "exit." corrected some menu items. starred menu items with functions in progress.

--- interface/interface_main.py
+++ interface/interface_main.py
@@ -1,30 +1,61 @@
 from models.kinematics import newtonian_model as nm
+from models.em import em_model as em
+
 
 print("\n\n--------------------------------------")
 print("Welcome to the Physics Problem Solver!")
-print("--------------------------------------")
-print("""[function name]\t\t:\t[code]\t:\t\t[required parameters]\n
-distance\t\t\t:\t(dis)\t:\tinitial velocity, acceleration, time
-final velocity\t\t:\t(fvd)\t:\tinitial velocity, acceleration, distance
-final velocity\t\t:\t(fvt)\t:\tinitial velocity, acceleration, time
-horizontal range\t:\t(hr)\t:\tinitial velocity, angle, gravity
-horizontal range\t:\t(hri)\t:\tinitial velocity, angle, initial height, gravity""")
-print("--------------------------------------\n")
+userFunc = ""
+while userFunc != "exit":
+    print("--------------------------------------")
+    print("[function name]\t\t:\t[code]\t:\t[required parameters]\n"
+    "distance\t\t\t:\tdis\t\t:\tinitial velocity, acceleration, time\n"
+    "final velocity\t\t:\tfvd\t\t:\tinitial velocity, acceleration, distance\n"
+    "final velocity\t\t:\tfvt\t\t:\tinitial velocity, acceleration, time\n"
+    "*horizontal range\t:\thr\t\t:\tinitial velocity, angle, gravity\n"
+    "*maximum height\t\t:\tmx\t\t:\tinitial velocity, angle, initial height, gravity\n"
+    "coulomb force\t\t:\tcf\t\t:\tcharge 1, charge 2, distance r\n"
+    "*electric field\t\t:\tef\t\t:\tcharge, distance r, x coordinate, y coordinate")
+    print("--------------------------------------\n")
 
-userFunc = input("Enter a function code:\n")
+    userFunc = input("Enter a function code:\n")
+    params = []
 
-params = []
-if userFunc == "distance":
-    for i in range(3):
-        params.append(int(input("Enter parameter %d\t" % (i+1))))
-    nm.distance(params[0], params[1], params[2])
+    #newtonian models
+    if userFunc == "dis":
+        for i in range(3):
+            params.append(int(input("Enter parameter %d\t" % (i+1))))
+        nm.distance(params[0], params[1], params[2])
 
-if userFunc == "fvd":
-    for i in range(3):
-        params.append(int(input("Enter parameter %d\t" % (i+1))))
-    nm.vel_final_dist(params[0], params[1], params[2])
+    if userFunc == "fvd":
+        for i in range(3):
+            params.append(int(input("Enter parameter %d\t" % (i+1))))
+        nm.vel_final_dist(params[0], params[1], params[2])
 
-if userFunc == "fvt":
-    for i in range(3):
-        params.append(int(input("Enter parameter %d\t" % (i+1))))
-    nm.vel_final_time(params[0], params[1], params[2])
+    if userFunc == "fvt":
+        for i in range(3):
+            params.append(int(input("Enter parameter %d\t" % (i+1))))
+        nm.vel_final_time(params[0], params[1], params[2])
+
+    if userFunc == "hr":
+        for i in range(3):
+            params.append(int(input("Enter parameter %d\t" % (i+1))))
+        nm.horizontal_range(params[0], params[1], params[2])
+
+    if userFunc == "mx":
+        for i in range(4):
+            params.append(int(input("Enter parameter %d\t" % (i+1))))
+        nm.maximum_height(params[0], params[1], params[2], params[3])
+
+    #em models
+    if userFunc == "cf":
+        for i in range(3):
+            params.append(input("Enter parameter %d\t" % (i+1)))
+        em.coulomb_force(float(params[0]), float(params[1]), float(params[2]))
+        #remove this eventually, just to check function
+        a = em.coulomb_force(float(params[0]), float(params[1]), float(params[2]))
+        print("%.4g" % a)
+
+    if userFunc == "ef":
+        for i in range(4):
+            params.append(int(input("Enter parameter %d\t" % (i+1))))
+        em.electric_field(params[0], params[1], params[2], params[3])
