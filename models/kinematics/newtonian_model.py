977899f61005c93166cfe0255ddaf401f54cf1a8
Alex Hopkins <hopkinsalex@knights.ucf.edu>
2018-10-25 19:02:42 -0400

    Created gravitational force function

--- models/kinematics/newtonian_model.py
+++ models/kinematics/newtonian_model.py
@@ -16,161 +16,180 @@ import numpy as np
 
 
 def distance(initial_velocity, acceleration, time):
-	"""
-	Calculates the distance traveled given the initial velocity, acceleration, and time then plots the distance traveled
-	with respect to time.
-	
-	:param initial_velocity: Integer initial velocity
-	:param acceleration: Integer acceleration
-	:param time: Integer time
-	
-	:return: Integer distance traveled
-	"""
-	
-	kplt.distance_plotter(initial_velocity, acceleration, time)
-	
-	return distance_helper(initial_velocity, acceleration, time)
+    """
+    Calculates the distance traveled given the initial velocity, acceleration, and time then plots the distance traveled
+    with respect to time.
+    
+    :param initial_velocity: Integer initial velocity
+    :param acceleration: Integer acceleration
+    :param time: Integer time
+    
+    :return: Integer distance traveled
+    """
+    
+    kplt.distance_plotter(initial_velocity, acceleration, time)
+    
+    return distance_helper(initial_velocity, acceleration, time)
 
 
 def distance_helper(initial_velocity, acceleration, time):
-	"""
-	Calculates the distance traveled given the initial velocity, acceleration, and time, Helper function to the distance
-	function.
-
-	:param initial_velocity: Integer initial velocity
-	:param acceleration: Integer acceleration
-	:param time: Integer time
-	
-	:return: Integer distance traveled
-	"""
-	dist = initial_velocity * time + 0.5 * (acceleration * time ** 2)
-	return dist
+    """
+    Calculates the distance traveled given the initial velocity, acceleration, and time, Helper function to the distance
+    function.
+    
+    :param initial_velocity: Integer initial velocity
+    :param acceleration: Integer acceleration
+    :param time: Integer time
+    
+    :return: Integer distance traveled
+    """
+    
+    dist = initial_velocity * time + 0.5 * (acceleration * time ** 2)
+    return dist
 
 
 def vel_final_dist(initial_velocity, acceleration, dist):
-	"""
-	Calculates the final velocity given the initial velocity, acceleration and the distance traveled. Then plots the
-	final velocity as a function of distance.
-	
-	:param initial_velocity: Integer initial velocity
-	:param acceleration: Integer acceleration
-	:param dist: Integer distance traveled
-	
-	:return: final velocity
-	"""
-	kplt.vel_final_dist_plotter(initial_velocity, acceleration, dist)
-
-	return vel_final_dist_helper(initial_velocity, acceleration, dist)
+    """
+    Calculates the final velocity given the initial velocity, acceleration and the distance traveled. Then plots the
+    final velocity as a function of distance.
+    
+    :param initial_velocity: Integer initial velocity
+    :param acceleration: Integer acceleration
+    :param dist: Integer distance traveled
+    
+    :return: final velocity
+    """
+    kplt.vel_final_dist_plotter(initial_velocity, acceleration, dist)
+    
+    return vel_final_dist_helper(initial_velocity, acceleration, dist)
 
 
 def vel_final_dist_helper(initial_velocity, acceleration, dist):
-	"""f
-	Calculates the final velocity given the initial velocity, acceleration and the distance traveled. And is a helper
-	function to be called by vel_final_dist()
-
-	:param initial_velocity: Integer initial velocity
-	:param acceleration: Integer acceleration
-	:param dist: Integer distance traveled
-
-	:return: final velocity
-	"""
-	vel = (initial_velocity ** 2 + 2 * acceleration * dist) ** 0.5
-	return vel
+    """
+    Calculates the final velocity given the initial velocity, acceleration and the distance traveled. And is a helper
+    function to be called by vel_final_dist()
+    
+    :param initial_velocity: Integer initial velocity
+    :param acceleration: Integer acceleration
+    :param dist: Integer distance traveled
+    
+    :return: final velocity
+    """
+    vel = (initial_velocity ** 2 + 2 * acceleration * dist) ** 0.5
+    return vel
 
 
 def vel_final_time(initial_velocity, acceleration, time):
-	"""
-	Calculates the final velocity given the initial velocity, acceleration, and time traveled. Then plots the final
-	velocity as a function of time.
-	
-	:param initial_velocity: Integer initial velocity
-	:param acceleration: Integer acceleration
-	:param time: Integer time spent in air
-	
-	:return: final velocity
-	"""
-	kplt.vel_final_time_plotter(initial_velocity, acceleration, time)
-
-	return vel_final_time_helper(initial_velocity, acceleration, time)
+    """
+    Calculates the final velocity given the initial velocity, acceleration, and time traveled. Then plots the final
+    velocity as a function of time.
+    
+    :param initial_velocity: Integer initial velocity
+    :param acceleration: Integer acceleration
+    :param time: Integer time spent in air
+    
+    :return: final velocity
+    """
+    kplt.vel_final_time_plotter(initial_velocity, acceleration, time)
+    
+    return vel_final_time_helper(initial_velocity, acceleration, time)
 
 
 def vel_final_time_helper(initial_velocity, acceleration, time):
-	"""
-	Calculates the final velocity given the initial velocity, acceleration, and time traveled. This is a helper function
-	for the vel_final_time function.
-
-	:param initial_velocity: Integer initial velocity
-	:param acceleration: Integer acceleration
-	:param time: Integer time spent in air
-
-	:return: final velocity
-	"""
-	vel = initial_velocity + acceleration * time
-	return vel
+    """
+    Calculates the final velocity given the initial velocity, acceleration, and time traveled. This is a helper function
+    for the vel_final_time function.
+    
+    :param initial_velocity: Integer initial velocity
+    :param acceleration: Integer acceleration
+    :param time: Integer time spent in air
+    
+    :return: final velocity
+    """
+    vel = initial_velocity + acceleration * time
+    return vel
 
 
 def horizontal_range(initial_velocity, angle, g=9.81):
-	"""
-	Find the range of the particle based on the angle of launch and initial velocity. Then plots the trajectory of the
-	particle.
-	
-	:param initial_velocity: Integer initial velocity
-	:param angle: Integer launch angle
-	:param g: Integer acceleration due to gravity
-
-	:return: Range
-	"""
-
-	kplt.trajectory_plotter(initial_velocity, angle, g)
-
-	return horizontal_range_helper(initial_velocity, angle, g)
+    """
+    Find the range of the particle based on the angle of launch and initial velocity. Then plots the trajectory of the
+    particle.
+    
+    :param initial_velocity: Integer initial velocity
+    :param angle: Integer launch angle
+    :param g: Integer acceleration due to gravity
+    
+    :return: Range
+    """
+    
+    kplt.trajectory_plotter(initial_velocity, angle, g)
+    
+    return horizontal_range_helper(initial_velocity, angle, g)
 
 
 def horizontal_range_helper(initial_velocity, angle, g=9.81):
-	"""
-	Find the range of the particle based on the angle of launch and initial velocity. This is a helper function for the
-	horizontal_range function.
-
-	:param initial_velocity: Integer initial velocity
-	:param angle: Integer launch angle in degrees
-	:param g: Integer acceleration due to gravity
-
-	:return: Range
-	"""
-
-	horizon_range = initial_velocity ** 2 * np.sin(2 * np.deg2rad(angle)) / g
-	return horizon_range
+    """
+    Find the range of the particle based on the angle of launch and initial velocity. This is a helper function for the
+    horizontal_range function.
+    
+    :param initial_velocity: Integer initial velocity
+    :param angle: Integer launch angle in degrees
+    :param g: Integer acceleration due to gravity
+    
+    :return: Range
+    """
+    
+    horizon_range = initial_velocity ** 2 * np.sin(2 * np.deg2rad(angle)) / g
+    return horizon_range
 
 
 def maximum_height(initial_velocity, angle, initial_height=0, g=9.81):
-	"""
-	Finds the maximum height of the particle given the launch angle and the initial velocity, then plots the trajectory
-	of the particle.
-
-	:param initial_velocity: Integer initial velocity
-	:param angle: Integer launch angle in degrees
-	:param initial_height: Integer initial height, default 0
-	:param g: Float acceleration due to gravity
-
-	:return: Max Height
-	"""
-
-	kplt.trajectory_plotter(initial_velocity, angle, g, initial_height)
-
-	return maximum_height_helper(initial_velocity, angle, g=g)
+    """
+    Finds the maximum height of the particle given the launch angle and the initial velocity, then plots the trajectory
+    of the particle.
+    
+    :param initial_velocity: Integer initial velocity
+    :param angle: Integer launch angle in degrees
+    :param initial_height: Integer initial height, default 0
+    :param g: Float acceleration due to gravity
+    
+    :return: Max Height
+    """
+    
+    kplt.trajectory_plotter(initial_velocity, angle, g, initial_height)
+    
+    return maximum_height_helper(initial_velocity, angle, g=g)
 
 
 def maximum_height_helper(initial_velocity, angle, initial_height=0, g=9.81):
-	"""
-	Finds the maximum height of the particle given the launch angle and the initial velocity. This is a helper function
-	to the maximum_height function.
+    """
+    Finds the maximum height of the particle given the launch angle and the initial velocity. This is a helper function
+    to the maximum_height function.
+    
+    :param initial_velocity: Integer initial velocity
+    :param angle: Integer launch angle in degrees
+    :param initial_height: Integer initial height, default 0
+    :param g: Integer acceleration due to gravity, default 9.81
+    
+    :return: Max Height
+    """
+    max_height = (initial_velocity ** 2 * np.sin(np.deg2rad(angle)) ** 2) / (g * 2)
+    return max_height
 
-	:param initial_velocity: Integer initial velocity
-	:param angle: Integer launch angle in degrees
-	:param initial_height: Integer initial height, default 0
-	:param g: Integer acceleration due to gravity, default 9.81
 
-	:return: Max Height
-	"""
-	max_height = (initial_velocity ** 2 * np.sin(np.deg2rad(angle)) ** 2) / (g * 2)
-	return max_height
+def gravitational_force(mass1, mass2, distance):
+    # TODO: Can make this more robust in the future by using distance as a vector.
+    """
+    Calculates the gravitational potential between two masses at some distance.
+    
+    :param mass1: scalar, mass in kg of first object
+    :param mass2: scalar, mass in kg of second object
+    :param distance: scalar, distance between two objects in meters
+    
+    :return force: scalar, Force of attraction between the objects in N
+    """
+    G = 6.674e-11  # N·kg^–2·m^2
+    
+    force = G * (mass1 * mass2) / distance
+
+    return force
